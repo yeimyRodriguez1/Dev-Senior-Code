63cecb788154c9a8a11dba77e81b23a548642778
Yeimy Rodríguez <yeimyd.rodrigueza@uqvirtual.edu.co>
2026-01-15 12:51:07 -0500

    Dev Senior Code

---
+++ Clases/lib/Modulo02/Clase02/PlataformaEstudiantil/Calificable.java
@@ -0,0 +1,5 @@
+package Clases.lib.Modulo02.Clase02.PlataformaEstudiantil;
+
+public interface Calificable {
+    void Calificar();
+}
---
+++ Clases/lib/Modulo02/Clase02/PlataformaStreaming/Reproducible.java
@@ -0,0 +1,5 @@
+package Clases.lib.Modulo02.Clase02.PlataformaStreaming;
+
+public interface Reproducible {
+    void reproducir();
+}
